cd7972e47e6f74cb1b40e2e70d17d53f77436748
pranavltim <pranav.supsande@ltimindtree.com>
2024-10-01 15:14:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
 provider "aws" {
+  access_key = "your aws access key here"
+  secret_key = "your aws secret key here"
+  region     = "ap-south-1"
+}
+
+
+resource "aws_instance" "ourfirst" {
+  ami           = "ami-08718895af4dfa033"
+  instance_type = "t2.micro"
+lifecycle {
+  prevent_destroy = true
+  ignore_changes = ["ami",]
+  }
+
+}
+
+
+
+provider "aws" {
   region = "ap-south-1"
 }
 
